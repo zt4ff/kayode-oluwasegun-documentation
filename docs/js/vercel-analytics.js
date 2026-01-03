2217d28cf04fad8471199444d3ea6f6eb3f14b40
Kayode Oluwasegun <segunkayode00@gmail.com>
2026-01-03 10:12:07 +0100

    fix: vercel analytics configuration

--- docs/js/vercel-analytics.js
+++ docs/js/vercel-analytics.js
@@ -2,9 +2,6 @@
   var script = document.createElement("script");
   script.src = "https://va.vercel-scripts.com/v1/script.js";
   script.defer = true;
-  script.setAttribute(
-    "data-domain",
-    "https://documentation-alpha-woad.vercel.app"
-  );
+  script.setAttribute("data-domain", "documentation-alpha-woad.vercel.app");
   document.head.appendChild(script);
 })();
