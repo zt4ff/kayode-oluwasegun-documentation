0e0c1afa4ef1028fbb48644fe9ae805ccf195ba6
Kayode Oluwasegun <segunkayode00@gmail.com>
2026-01-03 10:10:21 +0100

    add vercel analytics

---
+++ docs/js/vercel-analytics.js
@@ -0,0 +1,10 @@
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://va.vercel-scripts.com/v1/script.js";
+  script.defer = true;
+  script.setAttribute(
+    "data-domain",
+    "https://documentation-alpha-woad.vercel.app"
+  );
+  document.head.appendChild(script);
+})();
